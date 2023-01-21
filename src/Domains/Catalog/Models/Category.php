2dc68fb97a4924f52b0c582e34502af03353fe5a
Mdsujansarkar <sujansarkar786@gmail.com>
2023-01-22 00:08:43 +0600

    test

--- src/Domains/Catalog/Models/Category.php
+++ src/Domains/Catalog/Models/Category.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Castr\Domains\Shared\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Castr\Domains\Catalog\Models\Podcast;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -12,14 +13,25 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Category extends Model
 {
     use HasFactory;
-    protected $fillable = [];
-    protected $cast = [];
+    protected $fillable = [
+        'uuid',
+        'name'
+    ];
+
     public $timestamps = false;
+
     public function podcasts(): HasMany
     {
         return $this->hasMany(
             related: Podcast::class,
             foreignKey: 'category_id',
+        );
+    }
+
+    public function newEloquentBuilder($query): Builder
+    {
+        return new Builder(
+            query: $query,
         );
     }
 }
